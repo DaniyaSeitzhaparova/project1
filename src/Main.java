17d61dba45f75df97cf7934583a18bc040ec837d
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2023-12-19 10:10:36 +0600

    Classes and Objects

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,109 @@
 
-
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Student student =  new Student();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        student.setName("Maksat");
+        student.setSurname("Alimzhanov");
+        student.setAge(22);
+        student.setGpa(3.5);
+
+        System.out.println(student.toString());
+        Scanner scanner = new Scanner(System.in);
+        Student student1 = new Student(scanner.next(), scanner.next(), scanner.nextInt(), scanner.nextDouble());
+        System.out.println(student1.toString());
+    }
+
+    public class Student {
+    /*
+    public - accessible everywhere
+    private - accessible only inside of class
+    protected - accessible only inside same package
+    no modifiers - accessible only inside same package
+    */
+
+    /*
+    static - belongs to class creates once
+    non-static -  belongs to object creates to every object
+     */
+        //fields - param of the class
+
+        private static int id_gen = 1;
+        private int id;
+
+        private String surname;
+        private int age;
+        private double gpa;
+        private String name;
+
+        public Student(){  //no arg constructor
+            id = id_gen++;
+        }
+
+        public Student(String name, String surname, int age, double gpa){
+            this();
+            setName(name);
+            setSurname(surname);
+            setAge(age);
+            setGpa(gpa);
+        }
+        public int getId() {
+            return id;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setGpa(double gpa) {
+            this.gpa = gpa;
+        }
+
+        public double getGpa() {
+            return gpa;
+        }
+
+        @Override
+        public String toString() {
+            return "Student with id: " + getId()
+                    + "\nFullname: " + getName() + " " +getSurname()
+                    + "\nAge: " + getAge()
+                    + "\nGPA: " + getGpa();
         }
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
